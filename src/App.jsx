e3aed38dfa62e86b3d7feff9da69414e85d14147
wxnyeki <frankwanyeki01@gmail.com>
2026-05-13 00:14:25 -0700

    feat:frank/shell

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Navbar from "./components/Navbar";
+import CartSidebar from "./components/CartSidebar";
+import Hero from "./components/Hero";
+import Categories from "./components/Categories";
+import FeaturedProducts from "./components/FeaturedProducts";
+import Footer from "./components/Footer";
 
-export default function App() {
+function App() {
   return (
-    <div>
-      
+    <div className="flex min-h-screen bg-zinc-100 text-black">
+      <CartSidebar />
+
+      <main className="flex-1 p-6">
+        <Navbar />
+        <Hero />
+        <Categories />
+        <FeaturedProducts />
+        <Footer />
+      </main>
     </div>
-  )
+  );
 }
+
+export default App;
